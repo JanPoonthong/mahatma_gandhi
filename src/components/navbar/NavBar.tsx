96e950d62fab9c1eb30bcca1213d71f8823597e6
Jan Poonthong <janpoonthong628@gmail.com>
2024-06-09 10:18:36 +0700

    feat: nav bar

---
+++ src/components/navbar/NavBar.tsx
@@ -0,0 +1,14 @@
+function NavBar() {
+
+  return (
+    <>
+        <div className="flex gap-3 justify-end items-center">
+            <a className="bg-black text-white p-2 rounded" href="#">HOME</a>
+            <a href="#">BIO</a>
+            <a href="#">LEARN</a>
+        </div>
+    </>
+  )
+}
+
+export default NavBar 
